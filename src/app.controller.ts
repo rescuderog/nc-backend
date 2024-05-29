d3e6e7bc0036070e5aec607318b480f4062514f2
Ramiro Escudero <1389606+rescuderog@users.noreply.github.com>
2024-05-28 23:57:36 -0300

    endpoint protegido de prueba

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Controller()
 export class AppController {
@@ -9,5 +10,11 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
 
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('jwt')
+  getJwt(): string {
+    return 'Endpoint protegido, JWT correcto';
   }
 }
