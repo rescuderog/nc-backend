d74901affeb82dc46104e0fb4fe0abf8ffa9a475
Ramiro Escudero <1389606+rescuderog@users.noreply.github.com>
2024-06-04 23:27:55 -0300

    prueba de https

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
-  await app.listen(3000);
+  if (process.env.HTTPS_CONFIG === "ENABLED") {
+    const httpsOptions = {
+      key: fs.readFileSync('./secrets/private-key.pem'),
+      cert: fs.readFileSync('./secrets/public-certificate.pem'),
+    };
+
+    const app = await NestFactory.create(AppModule, {
+      httpsOptions,
+    });
+    app.useGlobalPipes(new ValidationPipe());
+    app.enableCors();
+    await app.listen(3000);
+  } else {
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe());
+    app.enableCors();
+    await app.listen(3000);
+  }
 }
 bootstrap();
